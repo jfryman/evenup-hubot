85e4e50c9a7f244d66eafea0c5a59aeecb1ec492
James Fryman <james@fryman.io>
2015-04-17 11:57:36 -0500

    Fixing nodejs declaration
    This commit changes the attribute for Class[nodejs] to `manage_repo`,
    which is correct as of `v0.7.1`.
    
    See: https://github.com/puppetlabs/puppetlabs-nodejs/blob/0.7.1/manifests/init.pp#L40

--- manifests/init.pp
+++ manifests/init.pp
@@ -148,7 +148,7 @@ class hubot (
     $service_enable_real = $service_enable
   }
   class { '::nodejs':
-    manage_package_repo => $nodejs_manage_repo,
+    manage_repo => $nodejs_manage_repo,
   }
 
   class { '::hubot::install': }
